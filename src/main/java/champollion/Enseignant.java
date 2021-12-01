3f44a3783521ead36e12efb28f7edcad4fa80290
Biinbin <gabin.lasservarie@etud.univ-jfc.fr>
2021-12-01 13:20:58 +0100

    FinTPMaven

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,38 +1,53 @@
 package champollion;
 
+import java.util.ArrayList;
+
 public class Enseignant extends Personne {
 
-    // TODO : rajouter les autres méthodes présentes dans le diagramme UML
+    public ArrayList<ServicePrevu> SP;
+    public ArrayList<Intervention> interventions;
 
     public Enseignant(String nom, String email) {
         super(nom, email);
     }
 
     /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
-     * de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut 0,75h
-     * "équivalent TD"
+     * Calcule le nombre total d'heures prévues pour cet enseignant en "heures
+     * équivalent TD" Pour le calcul : 1 heure de cours magistral vaut 1,5 h
+     * "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut
+     * 0,75h "équivalent TD"
      *
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
+     * @return le nombre total d'heures "équivalent TD" prévues pour cet
+     * enseignant, arrondi à l'entier le plus proche
      *
      */
     public int heuresPrevues() {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        double equivalentTD = 0;
+        for (ServicePrevu s : SP) {
+            equivalentTD = s.getVolumeCM() * 1.5 + s.getVolumeTD() + s.getVolumeTP() * 0.75;
+        }
+        return (int) equivalentTD;
     }
 
     /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE spécifiée en "heures équivalent TD" Pour
-     * le calcul : 1 heure de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure
-     * de TP vaut 0,75h "équivalent TD"
+     * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE
+     * spécifiée en "heures équivalent TD" Pour le calcul : 1 heure de cours
+     * magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent
+     * TD" 1 heure de TP vaut 0,75h "équivalent TD"
      *
      * @param ue l'UE concernée
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
+     * @return le nombre total d'heures "équivalent TD" prévues pour cet
+     * enseignant, arrondi à l'entier le plus proche
      *
      */
     public int heuresPrevuesPourUE(UE ue) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        double equivalentTD = 0;
+        for (ServicePrevu s : SP) {
+            if (ue == s.getue()) {
+                equivalentTD = s.getVolumeCM() * 1.5 + s.getVolumeTD() + s.getVolumeTP() * 0.75;
+            }
+        }
+        return (int) equivalentTD;
     }
 
     /**
@@ -44,8 +59,43 @@ public class Enseignant extends Personne {
      * @param volumeTP le volume d'heures de TP
      */
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        ServicePrevu SPrevu = new ServicePrevu(volumeCM, volumeTD, volumeTP, ue);
+        SP.add(SPrevu);
     }
 
-}
+    public boolean enSousService() {
+        return heuresPrevues() < 192;
+    }
+
+    public void ajouteIntervention(Intervention inter) {
+        interventions.add(inter);
+    }
+
+    public int resteAPlanifier(UE ue, TypeIntervention type) {
+        int sommePlanifiee = 0;
+
+        for (Intervention inter : interventions) {
+            if (inter.getType().equals(type) && inter.getUe().equals(ue)) {
+                sommePlanifiee += inter.getDuree();
+            }
+        }
+
+        for (ServicePrevu s : SP) {
+            if (s.getue().equals(ue)) {
+                switch (type) {
+                    case TP:
+                        sommePlanifiee -= s.getVolumeTP();
+                    case TD:
+                        sommePlanifiee -= s.getVolumeTD();
+                    case CM:
+                        sommePlanifiee -= s.getVolumeCM();
+                        break;
+                }
+            }
+
+        }
+        return (int) sommePlanifiee;
+
+    }
+
+    }
